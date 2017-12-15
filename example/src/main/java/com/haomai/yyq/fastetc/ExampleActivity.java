4eef09277014a257c14ef2db52ba9dba2dafa0c2
严雨祺 <750539565@qq.com>
2017-12-15 10:21:45 +0800

    first

--- example/src/main/java/com/haomai/yyq/fastetc/ExampleActivity.java
+++ example/src/main/java/com/haomai/yyq/fastetc/ExampleActivity.java
@@ -1,14 +1,7 @@
 package com.haomai.yyq.fastetc;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.Toast;
-
-import com.haomai.yyq.yyq.app.YyqApp;
 import com.haomai.yyq.yyq.app.activities.ProxyActivity;
 import com.haomai.yyq.yyq.app.delegates.YyqDelegate;
-import com.haomai.yyq.yyq.ec.icon.ECFontModule;
-import com.joanzapata.iconify.fonts.FontAwesomeModule;
 
 public class ExampleActivity extends ProxyActivity {
 
